e3ebadbc6e158f2d33a7c93e808b9d7a329b0295
Cesar Luis <cesarluis13915@gmail.com>
2020-06-29 20:32:33 -0600

    Use env variables to connect to db.

--- db.js
+++ db.js
@@ -1,8 +1,21 @@
 import mongoose from 'mongoose';
 
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '27017';
+const DB_NAME = process.env.DB_NAME || 'accapp';
+const DB_USER = process.env.DB_USER || 'admin';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'admin';
+
 mongoose.connect(
-  'mongodb://admin:admin@localhost:27017/accapp',
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     useNewUrlParser: true
   }
 );
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Unable to connect to db:'));
+db.once('open', function() {
+  console.log('Successfully connected to db.');
+});
